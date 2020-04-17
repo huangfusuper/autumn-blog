43b7d99e8a3b2bfe8a1d9ed1dba508f5b32fb1e1
huangfusuper <huangfusuper@163.com>
2020-04-17 15:33:39 +0800

    测试类修改

---
+++ autumn-blog-server/src/main/java/com/autumn/controller/TestController.java
@@ -0,0 +1,44 @@
+package com.autumn.controller;
+
+import com.autumn.utils.RedisCacheUtil;
+import com.autumn.utils.TokenUtil;
+import com.yunye.common.dto.UserLoginDto;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author huangfu
+ */
+@RestController
+public class TestController {
+    @Value("${token.secret}")
+    private String secret;
+    @Autowired
+    private TokenUtil tokenUtil;
+    @Autowired
+    private RedisCacheUtil redisCacheUtil;
+
+    @RequestMapping("test")
+    public UserLoginDto test(){
+        String token = tokenUtil.genToken();
+        System.out.println(token);
+        UserLoginDto cacheObject = redisCacheUtil.getCacheObject(token, UserLoginDto.class);
+        return cacheObject;
+    }
+
+    public static void main(String[] args) {
+        Claims claims = Jwts.parser()
+                //设置签名的秘钥
+                .setSigningKey("56eL5aSp5piv5LiA5Liq5b6I5aW955qE5a2j6IqCLOaIkeW+iOWWnOasou+8geS4jeWGt+S4jeeDre+8gQ==")
+                //设置需要解析的jwt
+                .parseClaimsJws("eyJhbGciOiJIUzI1NiJ9.eyJsb2dpblRva2VuIjoiZmFmODQ4NWMtMjA4Yi00OTVjLTg2NjAtYmRlZGUzOGIwODQyIiwic3ViIjoiYmxvZyIsImlhdCI6MTU4NzA0NDMzMSwianRpIjoiZGY1ZTUxYTMtNzhiNC00Mzk5LTg2MmEtMTI0ZjkxYzc0NDZjIn0.qX6eextIgsXrzibyqOxibDdZqsSethFPOhYBLKOPV68")
+                .getBody();
+        String loginToken = claims.get("loginToken").toString();
+
+        System.out.println(loginToken);
+    }
+}
